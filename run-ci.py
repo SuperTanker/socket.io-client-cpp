efd65bf40a99242459bb14d9bc661641b962cb97
Théo DELRIEU <theo.delrieu@tanker.io>
2018-12-05 17:45:26 +0100

    fix(requirements): bump ci commit hash [skip ci]

--- run-ci.py
+++ run-ci.py
@@ -8,6 +8,8 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
 
+    clean_cache_parser = subparsers.add_parser("clean-conan-cache")
+
     build_parser = subparsers.add_parser("deploy")
     build_parser.add_argument(
         "--profile", action="append", dest="profiles", required=True
@@ -15,20 +17,28 @@ def main() -> None:
     build_parser.add_argument("--user", default="tanker")
     build_parser.add_argument("--build-only", action="store_false", dest="upload")
     build_parser.add_argument("--ref", required=True)
+    build_parser.add_argument("--native-commit-hash", dest="native_commit_hash")
 
     args = parser.parse_args()
 
     ci.cpp.update_conan_config(sys.platform.lower())
-    if args.command == "deploy":
+    if args.command == "clean-conan-cache":
+        ci.cpp.clean_conan_cache()
+    elif args.command == "deploy":
+        if args.upload and not args.native_commit_hash:
+            sys.exit("--native-commit-hash must be passed when uploading")
+        if not args.upload and args.native_commit_hash:
+            sys.exit("--native-commit-hash cannot be used with --build-only")
         channel, version = args.ref.split("/")
         deployer = ci.cpp.Deployer(
             channel=channel,
             version=version,
             profiles=args.profiles, user=args.user,
         )
-        deployer.build()
-        if args.upload:
-            deployer.upload()
+        deployer.build(
+            upload=args.upload,
+            native_commit_hash=args.native_commit_hash,
+        )
     else:
         parser.print_help()
         sys.exit(1)
