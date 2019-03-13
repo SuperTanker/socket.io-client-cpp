d29c90ee892bf028874a04e70d723b39fe328be2
Théo DELRIEU <theo.delrieu@tanker.io>
2019-03-13 17:42:21 +0100

    use new CI files

--- run-ci.py
+++ run-ci.py
@@ -1,52 +1,23 @@
 import argparse
-import os
-import sys
+from path import Path
 
 import ci.cpp
-
-
-def make_sub_parser(subparsers, name):
-    parser = subparsers.add_parser(name)
-    parser.add_argument(
-        "--profile", action="append", dest="profiles", required=True
-    )
-    parser.add_argument("--user", default="tanker")
-    parser.add_argument("--ref", required=True)
-    return parser
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
 
-    subparsers.add_parser("clean-conan-cache")
-
-    build_parser = make_sub_parser(subparsers, "build")
-    deploy_parser = make_sub_parser(subparsers, "deploy")
+    subparsers.add_parser("build")
+    subparsers.add_parser("deploy")
 
     args = parser.parse_args()
     command = args.command
 
-    ci.cpp.update_conan_config(sys.platform.lower())
-    ci.cpp.set_build_env()
-    if command == "clean-conan-cache":
-        ci.cpp.clean_conan_cache()
-    elif command in ("build", "deploy"):
-        native_commit_hash = os.environ.get("SDK_NATIVE_COMMIT_HASH")
-        channel, version = args.ref.split("/")
-        deployer = ci.cpp.Deployer(
-            channel=channel,
-            version=version,
-            profiles=args.profiles, user=args.user,
-        )
-        should_upload = (command == "deploy")
-        deployer.build(
-            upload=should_upload,
-            native_commit_hash=native_commit_hash,
-        )
-    else:
-        parser.print_help()
-        sys.exit(1)
+    ci.cpp.update_conan_config()
+    should_upload = command == "deploy"
+    ci.cpp.set_home_isolation()
+    ci.cpp.build_recipe(Path.getcwd(), upload=should_upload, conan_reference=None)
 
 
 if __name__ == "__main__":
